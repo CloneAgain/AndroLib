09442e4ef69d74341a39c1ab5ec0d06c94f549fb
Santosh yadav <santosh@headerlabs.com>
2017-08-31 16:55:48 +0530

    working code for pushing android library to maven for version 1.0.1

--- app/src/main/java/com/hl/androlib/SanActivity.java
+++ app/src/main/java/com/hl/androlib/SanActivity.java
@@ -8,5 +8,7 @@ public class SanActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //https://gist.github.com/lopspower/6f62fe1492726d848d6d
     }
 }
